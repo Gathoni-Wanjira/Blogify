9f21cced331eae244096ae746bf2475ebb7ef367
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-03 00:50:56 +0300

    Categories Dropdown

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React from "react";
 
 function Modal() {
   return (
-
-<div className="addBlogSection" id="showAddBlog">
-    <form>
+    <div className="addBlogSection" id="showAddBlog">
+      <form>
         <p>New Blog Post</p>
-    <input type="text" name="title" id="title" placeholder="title"/><br/>
-            <input type="text" name="author" id="author" placeholder="author"/><br/>
-            <input type="url" name="url" id="url" placeholder="img-url"/><br/>
-            <input type="text" name="category" id="cat" placeholder="category"/><br/>
-            <input type="date" name="date-posted" id="date-posted"/><br/>
-             <input type="text" name="likes" id="likes" placeholder="likes"/><br/> 
-            <input type="text" name="comments" id="comments" placeholder="comments"/><br/> 
-            <input type="text" name="mins" id="mins-toread" placeholder="mins to read"/><br/>
-            <textarea name="blog" id="post" cols="30" rows="10" placeholder="blog post"></textarea><br/>
-            <input type="submit" value="Submit Blog Post"/>
-    </form>
+        <div className="table">
+            <div className="table-input">
+            <input type="text" name="title" id="title" placeholder="title" />
+         
+         <input type="url" name="url" id="url" placeholder="img-url" />
+        
+         <input type="date" name="date-posted" id="date-posted" />
+         
+         <input
+           type="text"
+           name="mins"
+           id="mins-toread"
+           placeholder=" X mins to read"
+         />
 
-</div>   
-  )
+            </div>
+
+         <div>
+<div className="sub-nav">
+<input type="text" name="category" id="cat" placeholder="category" />
+          
+          <input type="text" name="author" id="author" placeholder="author" />
+         
+</div>
+         
+          <textarea
+            name="blog"
+            id="post"
+            cols="30"
+            rows="10"
+            placeholder="blog post"
+          ></textarea>
+         <button  id="submitBlogbtn" type="submit" value="Submit Blog Post" >Submit Blog Post</button>
+
+         </div>
+            
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Modal
+export default Modal;
