90d3aee39e17ab54ad9143386ded029fa791e48b
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-02 11:23:09 +0300

    Populated blog cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import BlogCollection from './Components/BlogCollection'
+
 
 function App() {
+
+ 
+    const [blogposts, setblogposts] = useState([]);
+
+    
+    useEffect(() => {
+        fetch("http://localhost:3002/blogs")
+        .then(response => response.json())
+        .then(blogs => {
+            console.log(blogs)
+            setblogposts(blogs);
+        })
+    
+    },[])
+
+
   return (
     <div className="App">
-     <h2>Tester</h2>
+     <h2>Nav Bar Area</h2>
+     <BlogCollection blogposts={blogposts} />
     </div>
   );
 }
