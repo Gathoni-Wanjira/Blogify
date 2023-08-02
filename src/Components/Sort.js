5df6923f4ccab432bc9eb8b1fff891c8b375e6d4
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-02 20:20:29 +0300

    Sort Button

---
+++ src/Components/Sort.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sort() {
+  return (
+    <div className='sort'>
+        <button>Category</button>
+        <button>New</button>
+        <button>Top</button>
+    </div>
+  )
+}
+
+export default Sort
